b2d4e84f981fc06366e050b4a9bd194dcb115c0a
Marshall Houston <marshall.houston@gmail.com>
2017-10-31 16:42:15 -0600

    add friendly_id gem, create migration, run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031213428) do
+ActiveRecord::Schema.define(version: 20171031224017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,18 @@ ActiveRecord::Schema.define(version: 20171031213428) do
     t.datetime "updated_at", null: false
     t.bigint "category_id"
     t.index ["category_id"], name: "index_freelancers_on_category_id"
+  end
+
+  create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
   add_foreign_key "freelancers", "categories"
--- spec/features/categories/visitor_can_see_all_freelancers_on_category_show_spec.rb
+++ spec/features/categories/visitor_can_see_all_freelancers_on_category_show_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
+
 describe "As a Visitor" do
   describe "when I visit '/:CATEGORY_NAME'" do
     scenario "I can see all freelancers in the category" do
       category_1 = create(:category)
       category_2 = create(:category)
-      freelancer_1 = create(:freelancer, category: category_1)
-      freelancer_2 = create(:freelancer, category: category_2)
+      create(:freelancer, category: category_1)
+      create(:freelancer, category: category_2)
 
       visit category_path(category_1)
+
+      expect(current_path).to eq("/#{category_1.title}")
 
       expect(page).to have_content(category_1.freelancers.first.name)
       expect(page).to_not have_content(category_2.freelancers.first.name)
