535dd8a4543689c7de59c4855210a99c2a72ac56
BLaurenB <blbillington1@gmail.com>
2017-10-31 14:41:44 -0600

    updated changes that Marshall suggested. Implemented test for images on index page

--- spec/features/freelancers/guest_visits_freelancer_index_spec.rb
+++ spec/features/freelancers/guest_visits_freelancer_index_spec.rb
@@ -2,21 +2,22 @@ require 'rails_helper'
 
 
 describe "As a Visitor" do
-  describe "when I visit '/freelancers' I can all freelancers" do
+  describe "when I visit '/freelancers' I can see all freelancers" do
 
     scenario "I see existing freelancers" do
       freelancer = create_list(:freelancer, 3)
 
       visit freelancers_path
-      save_and_open_page
+
       expect(page).to have_content(freelancer[0].name)
       expect(page).to have_content(freelancer[2].name)
       expect(page).to have_content(freelancer[0].description)
       expect(page).to have_content(freelancer[2].description)
       expect(page).to have_content(freelancer[0].price)
       expect(page).to have_content(freelancer[2].price)
+      expect(page).to have_css("#image_1")
+      expect(page).to have_css("#image_3")
 
-      #We still need to check that the images appear
     end
 
 
