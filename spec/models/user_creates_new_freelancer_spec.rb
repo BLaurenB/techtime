cd8476384b780065ffc56921397e4ed3769cd753
BLaurenB <blbillington1@gmail.com>
2017-10-31 11:52:12 -0600

    wip

---
+++ spec/models/user_creates_new_freelancer_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "Freelancer can be created," do
+  it "has a name" do
+
+  end
+  it "has a description" do
+
+  end
+  it "has a price" do
+
+  end
+  it "has a name" do
+
+  end
+  it "has a name" do
+
+  end
+
+end
