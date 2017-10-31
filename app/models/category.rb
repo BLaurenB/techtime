1b8a60af37c8deedc6df08584e9a9b7f23f6c600
Emily Wise <wise.m.emily@gmail.com>
2017-10-31 15:33:53 -0600

    updated schema with correct category and freelancer relationship

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  has_many :freelancer
+  has_many :freelancers
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031210731) do
+ActiveRecord::Schema.define(version: 20171031213158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
-    t.bigint "freelancer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["freelancer_id"], name: "index_categories_on_freelancer_id"
   end
 
   create_table "freelancers", force: :cascade do |t|
@@ -30,7 +28,9 @@ ActiveRecord::Schema.define(version: 20171031210731) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_freelancers_on_category_id"
   end
 
-  add_foreign_key "categories", "freelancers"
+  add_foreign_key "freelancers", "categories"
 end
