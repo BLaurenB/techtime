7d3757d001c4bebafbbc7ee7413baf86ffde03d7
Marshall Houston <marshall.houston@gmail.com>
2017-10-31 22:02:07 -0600

    add failing test for cart path

--- spec/features/freelancers/visitor_can_view_cart_contents_spec.rb
+++ spec/features/freelancers/visitor_can_view_cart_contents_spec.rb
@@ -1,19 +1,20 @@
 require 'rails_helper'
 
-xdescribe "As a visitor" do
-  describe "When I click on the cart" do
+describe "As a visitor" do
+  describe "When I add a freelancer to the cart and then click on the cart" do
     before do
-        #Will need to add a category
-        freelancer = create(:freelancer)
-        visit freelancers_path
-        click_on "Cart"
+      #will need to add category
+      @freelancer = create(:freelancer)
+      visit freelancers_path
+      click_on "Choose Me"
+
+      click_on "Cart"
     end
 
-    scenario "I am redirected to a /cart page and see my cart contents" do
-
+    scenario "I am redirected to the /cart page and see my cart contents" do
+      expect(current_path).to eq('/cart')
     end
-
-
   end
-end 
- #When I click a link for the cart, And my current path should be “/cart” And I should see a small image, title, description and price for the item I just added And there should be a “total” price for the cart that should be the sum of all items in the cart
+end
+
+ # When I click “Add to cart” for that item And I click a link or button to view cart And my current path should be “/cart” And I should see a small image, title, description and price for the item I just added And there should be a “total” price for the cart that should be the sum of all items in the cart
