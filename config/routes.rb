a69cd7f3670efd852e261d387bb6412844a86569
BLaurenB <blbillington1@gmail.com>
2017-11-01 17:14:25 -0600

    category index page in order to show a list of categories for customers to choose from

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :cart, only: [:create]
   resources :freelancers, only: [:index]
+  resources :categories, only: [:index, :show]
 
   get '/cart', to: 'cart#show'
   get '/:id', to: 'categories#show'
