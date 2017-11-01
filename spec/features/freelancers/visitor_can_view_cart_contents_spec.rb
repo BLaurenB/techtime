ce9f0ccb1bbc6814e4b9da0110fc081db1eb3f70
Marshall Houston <marshall.houston@gmail.com>
2017-10-31 22:12:15 -0600

    add failing test/expectations for cart show

--- spec/features/freelancers/visitor_can_view_cart_contents_spec.rb
+++ spec/features/freelancers/visitor_can_view_cart_contents_spec.rb
@@ -12,9 +12,16 @@ describe "As a visitor" do
     end
 
     scenario "I am redirected to the /cart page and see my cart contents" do
+
       expect(current_path).to eq('/cart')
+      expect(page).to have_css('#image_1')
+      expect(page).to have_content(@freelancer.name)
+      expect(page).to have_content(@freelancer.description)
+      expect(page).to have_content(@freelancer.price)
     end
   end
 end
 
- # When I click “Add to cart” for that item And I click a link or button to view cart And my current path should be “/cart” And I should see a small image, title, description and price for the item I just added And there should be a “total” price for the cart that should be the sum of all items in the cart
+ # When I click “Add to cart” for that item And I click a link or button to view cart And my current path should be “/cart”
+
+ # And I should see a small image, title, description and price for the item I just added And there should be a “total” price for the cart that should be the sum of all items in the cart
