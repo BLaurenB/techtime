b8ac14e67203fb29848cb579abccc1ef8e6915cc
Emily Wise <wise.m.emily@gmail.com>
2017-11-02 19:27:22 -0600

    fixed test

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20171102224754) do
     t.integer "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "freelancers", "categories"
