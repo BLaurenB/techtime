b00aca98adc84a93e147f2d3b1c9052c9e58b613
Emily Wise <wise.m.emily@gmail.com>
2017-11-04 14:03:27 -0600

    added status field for freelancers

--- app/models/freelancer.rb
+++ app/models/freelancer.rb
@@ -3,4 +3,6 @@ class Freelancer < ApplicationRecord
   belongs_to :category
   has_many :order_freelancers
   has_many :orders, through: :order_freelancers
+
+  enum status: ["active", "retired"]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171104002256) do
+ActiveRecord::Schema.define(version: 20171104195215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20171104002256) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
+    t.integer "status", default: 0
     t.index ["category_id"], name: "index_freelancers_on_category_id"
   end
 
@@ -60,7 +61,7 @@ ActiveRecord::Schema.define(version: 20171104002256) do
     t.integer "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "freelancers", "categories"
