3c4694c9cd881caa83fcc85a08f3a5d0e777a680
BLaurenB <blbillington1@gmail.com>
2017-11-04 12:35:09 -0600

    user story 13 functionality completed

--- spec/features/orders/user_sees_order_details_spec.rb
+++ spec/features/orders/user_sees_order_details_spec.rb
@@ -12,24 +12,37 @@ describe "When I visit an order" do
 
       2.times {order.freelancers << freelancer_2}
       order.freelancers << freelancer_1
-    # require 'pry'; binding.pry
 
       visit order_path(order)
-      # save_and_open_page
 
       expect(page).to have_content("Paid")
       expect(page).to have_content("Date: #{order.created_at}")
-
-
 
       expect(page).to have_content("Name")
       expect(page).to have_content("Quantity: 1 hour")
       expect(page).to have_content("Subtotal: $150")
       expect(page).to have_content("Total: $450")
-
+      expect(page).to_not have_content("Updated on:")
 
       first(:link, "View Freelancer").click
       expect(current_path).to eq(freelancer_path(freelancer_1))
+    end
+
+    scenario "if the order is canceled or completed there is a different date shown" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      freelancer_1 = create(:freelancer)
+      order = user.orders.create(status: "Completed")
+      order.freelancers << freelancer_1
+
+      visit order_path(order)
+      # save_and_open_page
+
+      expect(page).to have_content("Completed")
+      expect(page).to have_content("Date: #{order.created_at}")
+      expect(page).to have_content("Updated on: #{order.updated_at}")
+
     end
 
 
