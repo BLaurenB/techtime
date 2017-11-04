22562cbbb822fb6831ac4bb86a2c7dd0bed3385a
Marshall Houston <marshall.houston@gmail.com>
2017-11-04 15:45:35 -0600

    move logic down into the order model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,12 +11,7 @@ class OrdersController < ApplicationController
   def create
     @user = current_user
     order = @user.orders.create
-    params[:cart_contents].each do |freelancer_id, quantity|
-      freelancer = Freelancer.find(freelancer_id.to_i)
-      quantity.to_i.times do
-        order.freelancers << freelancer
-      end
-    end
+    order.create_orders(params[:cart_contents])
     flash[:success] = "Order was successfully placed!"
     redirect_to orders_path
   end
