2d01cefd05a0591b12043174c9d95068be805729
Emily Wise <wise.m.emily@gmail.com>
2017-11-04 14:09:17 -0600

    Complete User story 14- updated freelancer to allow retired

--- spec/models/freelancer_spec.rb
+++ spec/models/freelancer_spec.rb
@@ -7,6 +7,24 @@ describe Freelancer do
     it { should validate_presence_of(:price) }
   end
 
+  describe "freelancer status" do
+    it "should default to active"do
+      freelancer = build(:freelancer)
+
+      expect(freelancer.status).to eq("active")
+      expect(freelancer.active?).to be_truthy
+    end
+
+    it "can be retired" do
+      freelancer = build(:freelancer, status: 1)
+
+      expect(freelancer.status).to eq("retired")
+      expect(freelancer.active?).to be_falsey
+      expect(freelancer.retired?).to be_truthy
+    end
+  end
+
+
   describe "relationships" do
     it "belongs to category" do
       freelancer = build(:freelancer)
