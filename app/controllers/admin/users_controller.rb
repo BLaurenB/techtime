7b5187ff3e49eab31adc0f926bab1ee9257063bd
Emily Wise <wise.m.emily@gmail.com>
2017-11-05 13:22:59 -0700

    added the edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,21 @@
 class Admin::UsersController < Admin::BaseController
 
-  def update
+  def edit
   end
+
+  def update
+    current_user.update(user_params)
+    flash[:success] = "You have successfully updated your account!"
+    redirect_to admin_user_path(current_user)
+  end
+
+  def show
+    @user = current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :company, :address2, :address1, :city, :state, :zip)
+  end  
 end
