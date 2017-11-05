c63ce9fe0777df9de117ac71eccc7309a2c633c7
BLaurenB <blbillington1@gmail.com>
2017-11-05 15:15:53 -0700

    Able to see counts of each status on admin dashboard

--- spec/features/orders/admin_can_view_orders_on_dashboard_spec.rb
+++ spec/features/orders/admin_can_view_orders_on_dashboard_spec.rb
@@ -2,20 +2,31 @@ require 'rails_helper'
 
 describe "As an admin" do
   describe "when I visit the dashboard," do
-    scenario "I see a list of all orders" do
+    before do
       admin = create(:user, role: 1)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-
-      orders = create_list(:order, 5)
-
+      @order_1 = create(:order, status: 0)
+      @order_2 = create(:order, status: 1)
+      @order_3 = create(:order, status: 2)
+      @order_4 = create(:order, status: 3)
+      @order_5 = create(:order, status: 0)
       visit admin_dashboard_path
+    end
 
-      expect(page).to have_content(orders[4].id)
-      expect(page).to have_content(orders[4].status)
-      expect(page).to have_content(orders[0].id)
-      expect(page).to have_content(orders[0].status)
-      expect(page).to have_content(orders[2].id)
-      expect(page).to have_content(orders[2].status)
+    scenario "I see a list of all orders" do
+
+      save_and_open_page
+      expect(page).to have_content(@order_1.id)
+      expect(page).to have_content(@order_1.status)
+      expect(page).to have_content(@order_4.id)
+      expect(page).to have_content(@order_4.status)
+    end
+
+    scenario "I can see the total number of orders for each status" do
+      expect(page).to have_content("Ordered: 2")
+      expect(page).to have_content("Paid: 1")
+      expect(page).to have_content("Canceled: 1")
+      expect(page).to have_content("Completed: 1")
     end
   end
 end
