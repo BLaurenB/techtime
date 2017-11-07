b6cfbfaaea5cc3de52e16380815808efe2b8b6fc
Marshall Houston <marshall.houston@gmail.com>
2017-11-06 20:38:06 -0700

    add test and functionality for profile info after user login

--- spec/features/users/registered_user_can_log_in_spec.rb
+++ spec/features/users/registered_user_can_log_in_spec.rb
@@ -10,15 +10,12 @@ describe "As a registered user" do
       expect(current_path).to eq('/login')
     end
 
-    scenario "I can fill in my credentials" do
-
+    scenario "I can login and see my profile" do
       user = User.create(username: 'normaluser', password: 'password', email: "email@email.com", full_name: "Bob Smith", company: "Company", address1: "Address1", city: "Denver", state: "CO", zip:"80303", role: 0)
 
       visit '/'
 
       click_link 'Login'
-
-
 
       fill_in "session[username]", with: user.username
       fill_in "session[password]", with: user.password
@@ -29,8 +26,15 @@ describe "As a registered user" do
       expect(page).to_not have_content("Login")
       expect(page).to have_content("Logout")
 
-
+      within ".profile" do
+        expect(page).to have_content("email@email.com")
+        expect(page).to have_content("Bob Smith")
+        expect(page).to have_content("Company")
+        expect(page).to have_content("Address1")
+        expect(page).to have_content("Denver")
+        expect(page).to have_content("CO")
+        expect(page).to have_content("80303")
+      end
     end
   end
-
 end
