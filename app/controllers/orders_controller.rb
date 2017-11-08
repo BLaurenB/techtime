99690b0404dfc1d94cf083606e8379f9a134f3ee
Marshall Houston <marshall.houston@gmail.com>
2017-11-08 12:57:32 -0700

    reset session[:cart] after clicking checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,16 @@
 class OrdersController < ApplicationController
 
-
-
   def index
     @user = current_user
   end
 
   def show
-
     if current_user.id != Order.find(params[:id]).user_id
       render file: "public/404"
     else
       @order = current_user.orders.find(params[:id])
       @freelancers = @order.freelancers
     end
-
   end
 
   def create
@@ -22,9 +18,8 @@ class OrdersController < ApplicationController
     order = @user.orders.create
     order.create_orders(params[:cart_contents])
     flash[:success] = "Order was successfully placed!"
+    session[:cart].clear
     redirect_to orders_path
   end
-
-
 
 end
