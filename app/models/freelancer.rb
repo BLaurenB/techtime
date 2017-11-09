d89124984b26079177de0ae9b1b259154ada90f6
Emily Wise <wise.m.emily@gmail.com>
2017-11-08 21:33:02 -0700

    minor adjustments to freelancer price and catergory index width

--- app/models/freelancer.rb
+++ app/models/freelancer.rb
@@ -1,6 +1,7 @@
 class Freelancer < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  validates :description, :price, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
   belongs_to :category
   has_many :order_freelancers
   has_many :orders, through: :order_freelancers
