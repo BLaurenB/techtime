7ac78d658e913a62c066cda0028f8523e3a6acaa
Emily Wise <wise.m.emily@gmail.com>
2017-11-21 16:07:13 -0700

    wip routes for prework

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,29 @@
 Rails.application.routes.draw do
-  root to: "welcome#index"
+  get '/', to: 'welcome#index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/dashboard', to: 'users#show'
+  get '/logout', to: 'sessions#destroy'
 
-  resources :cart, only: [:create]
-  resources :freelancers, only: [:index, :show]
-  resources :categories, only: [:index, :show]
-  resources :users, only: [:new, :create, :show]
-  resources :orders, only: [:index, :show, :create]
+  # root to: "welcome#index"
+  post '/cart', to: 'cart#create', as: 'cart_index'
+   #resources :cart, only: [:create]
+
+  #resources :freelancers, only: [:show]
+  get '/freelancers', to: 'freelancers#index'
+  get '/freelancers/:id', to: 'freelancers#show', as: 'freelancer'
+  get '/categories', to: 'categories#index'
+  get '/:id', to: 'categories#show', as: 'category'
+
+  #resources :categories, only: [ :show]
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/orders', to: 'orders#index'
+  get '/orders', to: 'orders#show', as: 'order'
+  post '/orders', to: 'order#create'
+  #resources :users, only: [:new, :create, :show]
+  #resources :orders, only: [:index, :show, :create]
 
   namespace :admin do
     resources :users, only: [:edit, :update, :show]
@@ -16,14 +34,11 @@ Rails.application.routes.draw do
     patch '/dashboard', to: 'orders#update'
   end
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  get '/dashboard', to: 'users#show'
-  get '/logout', to: 'sessions#destroy'
+
 
   get '/cart', to: 'cart#show'
   patch '/cart', to: 'cart#update'
-  get '/:id', to: 'categories#show'
+  #get '/:id', to: 'categories#show'
   delete '/cart', to: 'cart#destroy'
 
 
